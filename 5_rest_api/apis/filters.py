e2f5987a95803c945c4e42c4ce2f67af092fe3c0
Chanawit.k <chanawit.kaewpaluek@gmail.com>
2024-05-18 04:28:50 +0700

    - add filter School

--- 5_rest_api/apis/filters.py
+++ 5_rest_api/apis/filters.py
@@ -1,4 +1,10 @@
-from django_filters import FilterSet, filters
+from django_filters import rest_framework as filters  # type: ignore
+from .models import School
 
 
-# code here
+class SchoolFilter(filters.FilterSet):
+    name = filters.CharFilter(field_name='name', lookup_expr='icontains')
+
+    class Meta:
+        model = School
+        fields = ['name']
--- 5_rest_api/apis/views/v1/school.py
+++ 5_rest_api/apis/views/v1/school.py
@@ -1,12 +1,14 @@
 from rest_framework import viewsets  # type: ignore
 from apis import serializers
 from apis.models import School
+from apis.filters import SchoolFilter
 
 
 class SchoolViewSet(viewsets.ModelViewSet):
     """View for manage recipe APIs."""
-    serializer_class = serializers.SchoolSerializer
+    serializer_class = serializers.SchoolDetailSerializer
     queryset = School.objects.all()
+    filterset_class = SchoolFilter
     # authentication_classes = [TokenAuthentication]
     # permission_classes = [IsAuthenticated]
 
@@ -14,12 +16,11 @@ class SchoolViewSet(viewsets.ModelViewSet):
     #     """Retrieve recipes for authenticated user."""
     #     return self.queryset.filter(user=self.request.user).order_by('-id')
 
-    # def get_serializer_class(self):
-    #     """Return the serializer class for request."""
-    #     if self.action == 'list':
-    #         return serializers.RecipeSerializer
-    #     print(self.action)
-    #     return self.serializer_class
+    def get_serializer_class(self):
+        """Return the serializer class for request."""
+        if self.action == 'list':
+            return serializers.SchoolSerializer
+        return self.serializer_class
 
     # def perform_create(self, serializer):
     #     """Create a new recipe."""
