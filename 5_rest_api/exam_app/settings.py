7366ba38df307a4f65743e4cb2f6d618022ba6a3
Chanawit.k <chanawit.kaewpaluek@gmail.com>
2024-05-17 22:55:49 +0700

    - config swagger

--- 5_rest_api/exam_app/settings.py
+++ 5_rest_api/exam_app/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'django_filters',
-    'apis.apps.ApisConfig'
+    'apis.apps.ApisConfig',
+    'drf_spectacular',
 ]
 
 MIDDLEWARE = [
@@ -144,3 +145,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
