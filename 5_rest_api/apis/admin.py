cc42f2134a3f8d22b9329b79e8c62f1981e5a37d
Chanawit.k <chanawit.kaewpaluek@gmail.com>
2024-05-17 22:55:04 +0700

    - admin page

--- 5_rest_api/apis/admin.py
+++ 5_rest_api/apis/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from apis.models import School, ClassRoom, Teacher, Student
 # Register your models here.
+admin.site.register(School)
+admin.site.register(ClassRoom)
+admin.site.register(Teacher)
+admin.site.register(Student)
