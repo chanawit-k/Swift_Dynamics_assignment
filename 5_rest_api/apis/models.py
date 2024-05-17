4a1f375e57633ee5131bd8f458bca192a3ad450b
Chanawit.k <chanawit.kaewpaluek@gmail.com>
2024-05-17 16:37:00 +0700

    - add model

--- 5_rest_api/apis/models.py
+++ 5_rest_api/apis/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
-# Create your models here.
+
+class Teacher(models.Model):
+    first_name = models.CharField(max_length=225)
+    last_name = models.CharField(max_length=225)
+    sex = models.CharField(max_length=225)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+
+class ClassRoom(models.Model):
+    year = models.PositiveIntegerField()
+    section = models.CharField(max_length=10)
+    teachers = models.ManyToManyField(Teacher, related_name='classrooms')
+
+    def __str__(self):
+        return f"Year {self.year} Section {self.section}"
+
+
+class Student(models.Model):
+    first_name = models.CharField(max_length=225)
+    last_name = models.CharField(max_length=225)
+    sex = models.CharField(max_length=225)
+    classroom = models.ForeignKey(ClassRoom, on_delete=models.CASCADE, related_name='students')
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+
+class School(models.Model):
+    name = models.CharField(max_length=225)
+    abbreviation = models.CharField(max_length=225)
+    address = models.CharField(max_length=255)
+    classrooms = models.ManyToManyField(ClassRoom, related_name='schools')
+
+    def __str__(self):
+        return self.name
