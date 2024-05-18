b2433c7a298302f194405d6714b501244ed09526
Chanawit.k <chanawit.kaewpaluek@gmail.com>
2024-05-18 12:56:34 +0700

    - filter Student + fixed serach school

--- 5_rest_api/apis/filters.py
+++ 5_rest_api/apis/filters.py
@@ -1,5 +1,5 @@
-from django_filters import rest_framework as filters  # type: ignore
-from .models import School, ClassRoom, Teacher
+from django_filters import rest_framework as filters
+from .models import School, ClassRoom, Teacher, Student
 
 
 class SchoolFilter(filters.FilterSet):
@@ -20,8 +20,7 @@ class ClassRoomFilter(filters.FilterSet):
 
 
 class TeacherFilter(filters.FilterSet):
-    school = filters.ModelChoiceFilter(queryset=School.objects.all(),
-                                       field_name='schools')
+    school = filters.CharFilter(method='filter_by_school')
     classroom = filters.ModelChoiceFilter(queryset=ClassRoom.objects.all(),
                                           field_name='classrooms')
     first_name = filters.CharFilter(field_name='first_name',
@@ -33,3 +32,23 @@ class TeacherFilter(filters.FilterSet):
     class Meta:
         model = Teacher
         fields = ['school', 'classroom', 'first_name', 'last_name', 'gender']
+
+    def filter_by_school(self, queryset, name, value):
+        return queryset.filter(classrooms__schools__id=value).distinct()
+
+
+class StudentFilter(filters.FilterSet):
+    school = filters.CharFilter(method='filter_by_school')
+    classroom = filters.CharFilter(field_name='classroom', lookup_expr='exact')
+    first_name = filters.CharFilter(field_name='first_name',
+                                    lookup_expr='icontains')
+    last_name = filters.CharFilter(field_name='last_name',
+                                   lookup_expr='icontains')
+    gender = filters.CharFilter(field_name='gender', lookup_expr='icontains')
+
+    class Meta:
+        model = Student
+        fields = ['school', 'classroom', 'first_name', 'last_name', 'gender']
+
+    def filter_by_school(self, queryset, name, value):
+        return queryset.filter(classroom__schools__id=value).distinct()
